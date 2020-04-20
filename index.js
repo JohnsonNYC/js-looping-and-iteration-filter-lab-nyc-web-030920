2c3cd6a060d0ccdc3f5397e1a20564e102672a89
JohnsonNYC <jkow95@gmail.com>
2020-04-19 20:09:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file
+function findMatching(list,name){ 
+    return list.filter(function(driverName){
+        return driverName.toLowerCase() == name.toLowerCase();
+    })
+}
+
+// function fuzzyMatch(list,name){
+//     return list.filter(function(driverName){
+//          if (driverName[0] == name){
+//              return driverName
+//          }
+//     })
+// }
+function fuzzyMatch (list, partialName) {
+    let lengthOfName = partialName.length;
+    return list.filter(function (driverName) {
+      return driverName.slice(0, lengthOfName) === partialName;
+    });
+}
+
+function matchName (list, name) {
+    return list.filter(function (driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }
